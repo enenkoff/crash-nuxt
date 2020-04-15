d369e284db42f03350788e4251cd36f7acba8ebf
enenkov <enenkoff@gmail.com>
2020-04-16 01:33:57 +0300

    test fetch from quwi API

--- store/users.js
+++ store/users.js
@@ -10,10 +10,18 @@ export const mutations = {
 
 export const actions = {
   async fetch({commit}) {
-    const users = await this.$axios.$get('https://jsonplaceholder.typicode.com/users')
+    let users;
+    await this.$axios.get('https://api.quwi.com/v2/projects', {
+      headers: {
+        // Authorization: 'Bearer ' + state.userToken
+        Authorization: 'Bearer 2ec994fb9a453c489ba98d607c5f111a'
+      }
+    }).then((response) => {
+      users = response.data.projects
+    });
     commit('setUsers', users)
   }
-}
+};
 
 export const getters = {
   users: s => s.users
